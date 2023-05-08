cf968b720662c13bcad9905b554a9858fea0e249
Nicolas Liendro <nicolasliendro0697@gmail.com>
2023-05-08 09:10:21 -0300

    Update models imports and classes

--- app/models.py
+++ app/models.py
@@ -1,24 +1,30 @@
-from django.db import models
+from django.db import models as models_django
+from django.contrib.gis.db import models as models_gis
+from django.contrib.auth.models import User
 
 
-class Cobertura(models.Model):
+class Direccion(models_django.Model):
+    direccion = models_django.CharField(max_length=100)
+    ciudad = models_django.CharField(max_length=50)
 
-    color = models.CharField(blank=True, null=True, max_length=100)
-    location = models.GeometryField(blank=False, null=False)
+    def __str__(self):
+        return self.direccion
+
+class Cobertura(models_django.Model):
+    color = models_django.CharField(blank=True, null=True, max_length=100)
+    location = models_gis.GeometryField(blank=False, null=False)
 
     def __str__(self):
         return f"Cobertura {self.pk}"
 
     class Meta:
         db_table = "address"
-        order_with_respect_to = "address"
 
+class Plan(models_django.Model):
 
-class Plan(models.Model):
-
-    nombre =  models.TextField()
-
+    nombre =  models_django.TextField()
+    coberturas = models_django.ManyToManyField(Cobertura)
+    direcciones = models_django.ManyToManyField(Direccion)
     def __str__(self):
         return f"Plan {self.nombre}"
-
 
