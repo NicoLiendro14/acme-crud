1978eda5c553d3254e2187ddda24c75c8ec0eabc
Nicolas Liendro <nicolasliendro0697@gmail.com>
2023-05-08 15:52:52 -0300

    Refactor imports

--- app/views.py
+++ app/views.py
@@ -1,18 +1,13 @@
 import json
 
 from rest_framework import generics
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.serializers import AuthTokenSerializer
 
 
 from .serializers import (
